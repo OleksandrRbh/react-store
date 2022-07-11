ddf1697b53ddf23b19bff6cd1fee8c492f7e4ac2
Oleksandr Riabukha <a.riabukha@freshdesign.ua>
2022-07-11 21:40:02 +0300

    feature(app): add card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './index.css';
+import { ReactComponent as IStar } from './../../img/icons/i-star.svg'
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: props.product
+    }
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card__content">
+          <img src={this.state.product.images[0]} className="card__image" alt='product' />
+
+            <div className="card__info">
+              <div className="info-header">
+                <button className="info-raiting card__button">
+                  <span>{this.state.product.rating}</span>
+                  <IStar />
+                </button>
+
+                <span className="info-price">{this.state.product.price}</span>
+              </div>
+
+              <div className="info-title">{this.state.product.title}</div>
+
+              <div className="info-category">{this.state.product.category}</div>
+            </div>
+        </div>
+
+        <button className="card__action-button card__button" data-element="addToCart">Add To Cart</button>
+      </div>
+    );
+  }
+}
+export default Card
